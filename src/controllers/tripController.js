2969645c6e6c168609735c71678ff9277aeb3232
Adrian D'Souza <dsouzaadrian24@gmail.com>
2020-10-02 21:07:46 -0400

    Removed method code

--- src/controllers/tripController.js
+++ src/controllers/tripController.js
@@ -12,42 +12,7 @@ import { isEmpty, dateIsPast } from '../helpers/validations';
  * @returns {object} reflection object
  */
 const addNewTrip = async (req, res) => {
-    const { tripId, name, origin, destination, date } = req.body;
-    const { userId } = req.params;
-
-    if (isEmpty(name) || isEmpty(origin) || isEmpty(destination) || isEmpty(date)) {
-        errorMessage.error = 'Name, origin, destination and date fields cannot be empty';
-        return res.status(status.bad).send(errorMessage);
-    }
-
-    if (isNaN(Date.parse(date)) || dateIsPast(Date.parse(date))) {
-        errorMessage.error = 'Invalid Date - either not a date or date given is in the past';
-        return res.status(status.bad).send(errorMessage);
-    }
-
-    const tripDate = moment(Date.parse(date));
-    const insertQuery = `INSERT INTO trips 
-    (trip_id, user_id, name, origin, destination, date) VALUES (
-        $1, $2, $3, $4, $5, $6
-    ) RETURNING *`;
-    const values = [
-        tripId,
-        userId,
-        name,
-        origin,
-        destination,
-        tripDate,
-    ];
-
-    try {
-        const { rows } = await pool.query(insertQuery, values);
-        const response = rows[0];
-        successMessage.data = response;
-        return res.status(status.created).send(successMessage);
-    } catch (error) {
-        errorMessage.error = error;
-        return res.status(status.bad).send(errorMessage);
-    }
+    
 }
 
 
@@ -58,25 +23,7 @@ const addNewTrip = async (req, res) => {
  * @returns {object} reflection object
  */
 const getAllTrips = async (req, res) => {
-    const { userId } = req.params;
-    const fetchQuery = `SELECT * FROM trips WHERE user_id=$1`;
-    const values = [userId];
-
-    try {
-        const { rows, rowCount } = await pool.query(fetchQuery, values);
-        if (rowCount === 0) {
-            errorMessage.error = 'No results found';
-            return res.status(status.nocontent).send(errorMessage);
-        }
-
-        const response = rows;
-        successMessage.data = response;
-        successMessage.count = rowCount;
-        return res.status(status.success).send(successMessage);
-    } catch (error) {
-        errorMessage.error = error;
-        return res.status(status.bad).send(errorMessage);
-    }
+    
 }
 
 /**
@@ -86,24 +33,7 @@ const getAllTrips = async (req, res) => {
  * @returns {object} reflection object
  */
 const getSpecificTrip = async (req, res) => {
-    const { tripId, userId } = req.params;
-    const fetchQuery = `SELECT * FROM trips WHERE id=$1 AND user_id = $2`;
-    const values = [tripId, userId];
-
-    try {
-        const { rows, rowCount } = await pool.query(fetchQuery, values);
-        if (rowCount === 0) {
-            errorMessage.error = 'Trip not found';
-            return res.status(status.notfound).send(errorMessage);
-        }
-
-        const response = rows[0];
-        successMessage.data = response;
-        return res.status(status.success).send(successMessage);
-    } catch (error) {
-        errorMessage.error = error;
-        return res.status(status.bad).send(errorMessage);
-    }
+    
 }
 
 /**
@@ -113,35 +43,7 @@ const getSpecificTrip = async (req, res) => {
  * @returns {object} reflection object
  */
 const updateTrip = async (req, res) => {
-    const { userId, tripId } = req.params;
-    const { name, origin, destination, completed, date } = req.body;
-
-    if (isEmpty(name) || isEmpty(origin) || isEmpty(destination) || isEmpty(date)) {
-        errorMessage.error = 'Name, origin, destination and date fields cannot be empty';
-        return res.status(status.bad).send(errorMessage);
-    }
-
-    if (isNaN(Date.parse(date))) {
-        errorMessage.error = 'Invalid Date';
-        return res.status(status.bad).send(errorMessage);
-    }
-
-    const tripDate = moment(Date.parse(date));
-
-    const updateQuery = `UPDATE trips 
-    SET name=$1, origin=$2, destination=$3, completed=$4, date=$5 
-    WHERE id=$6 AND user_id=$7 RETURNING *`;
-    const values = [name, origin, destination, completed, tripDate, tripId, userId];
-
-    try {
-        const { rows } = await pool.query(updateQuery, values);
-        const response = rows[0];
-        successMessage.data = response;
-        return res.status(status.success).send(successMessage);
-    } catch (error) {
-        errorMessage.error = error;
-        return res.status(status.bad).send(errorMessage);
-    }
+    
 }
 
 /**
@@ -151,23 +53,7 @@ const updateTrip = async (req, res) => {
  * @returns {object} reflection object
  */
 const deleteTrip = async (req, res) => {
-    const { tripId } = req.params;
-    const deleteQuery = `DELETE FROM trips WHERE id=$1 RETURNING id, user_id`;
-    const values = [tripId];
-    try {
-        const { rows, rowCount } = await pool.query(deleteQuery, values);
-        if (rowCount === 0) {
-            errorMessage.error = 'No records changed';
-            return res.status(status.bad).send(errorMessage);
-        }
-
-        const response = rows[0];
-        successMessage.data = response;
-        return res.status(status.success).send(successMessage);
-    } catch (error) {
-        errorMessage.error = error;
-        return res.status(status.bad).send(errorMessage);
-    }
+    
 }
 
 export {
--- src/routes/tripRoute.js
+++ src/routes/tripRoute.js
@@ -4,10 +4,10 @@ import { deleteUser } from '../controllers/userController';
 import { validateUser, validateTrip } from '../middleware/validate';
 const router = express.Router();
 
-router.get('/:userId/trips', validateUser, getAllTrips);
-router.get('/:userId/trips/:tripId', validateUser, validateTrip, getSpecificTrip);
-router.post('/:userId/trips/create', validateUser, addNewTrip);
-router.put('/:userId/trips/:tripId/update', validateUser, validateTrip, updateTrip);
-router.delete('/:userId/trips/:tripId/delete', validateUser, validateTrip, deleteUser);
+router.get('/trips', validateUser, getAllTrips);
+router.get('/trips', validateUser, validateTrip, getSpecificTrip);
+router.post('/trips/create', validateUser, addNewTrip);
+router.put('/trips/update', validateUser, validateTrip, updateTrip);
+router.delete('/trips/delete', validateUser, validateTrip, deleteUser);
 
 export default router;
