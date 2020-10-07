8b9d132f1280d0a9bdf9fc98c69ac0155e0ed33f
Adrian D'Souza <dsouzaadrian24@gmail.com>
2020-10-06 22:40:13 -0400

    Item route and controller structure

---
+++ src/controllers/itemController.js
@@ -0,0 +1,43 @@
+import pool from '../database/pool';
+import { errorMessage, status, successMessage } from '../helpers/status';
+import {isEmpty} from "../helpers/validations";
+
+/**
+ * Get all items function
+ * @param {Request} req
+ * @param {Response} res
+ * @returns {object} reflection object
+ */
+export const getItems = async function(req, res) {}
+
+/**
+ * Get all items function
+ * @param {Request} req
+ * @param {Response} res
+ * @returns {object} reflection object
+ */
+export const getItem = async function(req, res) {}
+
+/**
+ * Get all items function
+ * @param {Request} req
+ * @param {Response} res
+ * @returns {object} reflection object
+ */
+export const createItem = async function(req, res) {}
+
+/**
+ * Get all items function
+ * @param {Request} req
+ * @param {Response} res
+ * @returns {object} reflection object
+ */
+export const updateItem = async function(req, res) {}
+
+/**
+ * Get all items function
+ * @param {Request} req
+ * @param {Response} res
+ * @returns {object} reflection object
+ */
+export const deleteItem = async function(req, res) {}
---
+++ src/routes/itemRoute.js
@@ -0,0 +1,36 @@
+import express from 'express';
+const router = express.Router();
+
+/**
+ * Get all items pertaining to a trip
+ * Params: {userId, tripId, bagId}
+ */
+router.get('/items/:userId/:tripId/:bagId/items');
+
+/**
+ * Get specific item
+ * Params: {userId, tripId, bagId, itemId}
+ */
+router.get('/items/:userId/:tripId/:bagId/items/:itemId');
+
+/**
+ * Create new item for bag
+ * Param: {userId, tripId, bagId}
+ * Body: {description, type}
+ */
+router.post('/items/:userId/:tripId/:bagId/items');
+
+/**
+ * Update item in bag
+ * Param: {userId, tripId, bagId, itemId}
+ * Body: {description, type}
+ */
+router.put('/items/:userId/:tripId/:bagId/items/:itemId');
+
+/**
+ * Delete item from bag
+ * Param: {userId, tripId, bagId, itemId}
+ */
+router.delete('/items/:userId/:tripId/:bagId/items/:itemId');
+
+export default router;
