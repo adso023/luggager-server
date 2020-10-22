5677652c683719648d7a0707b3b34a492a1cc330
Adrian D'Souza <dsouzaadrian24@gmail.com>
2020-10-22 15:43:35 -0400

    Testing it with Heroku

--- src/database/pool.js
+++ src/database/pool.js
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 import config from '../../env';
 
-const databaseConfig = { connectionString: config.database_url };
+const databaseConfig = { connectionString: config.database_url, ssl: {rejectUnauthorized: false }};
 const pool = new Pool(databaseConfig);
 
 export default pool;
